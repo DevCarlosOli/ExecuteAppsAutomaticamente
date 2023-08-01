e2821d180f369a3254416a47b2b79d42e83c76ed
Carlos Oliveira <74160616+DevCarlosOli@users.noreply.github.com>
2023-08-01 17:56:07 -0300

    Add files via upload

---
+++ ExecuteAppsAutomaticamente/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ExcuteAppsAutomaticamente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// Caminhos dos .exe do Google Chrome e do Visual Studio
+			string caminhoChrome = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
+			string caminhoVS = @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe";
+
+			try
+			{
+				// Abrir o Google Chrome
+				Process.Start(caminhoChrome);
+
+				// Intervalo para que o Chrome possa abrir.
+				Thread.Sleep(2000);
+
+				// Abrir o Visual Studio 2022
+				Process.Start(caminhoVS);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Ocorreu um erro ao tentar abrir os programas!");
+                Console.WriteLine(ex.Message);
+			}
+
+        }
+    }
+
+}
